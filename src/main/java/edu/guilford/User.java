888b8d13390fd887e294886f02e4e9ba1a8ab5b6
emendyk <emendyk@guilford.edu>
2023-04-22 20:45:07 -0400

    user

---
+++ src/main/java/edu/guilford/User.java
@@ -0,0 +1,25 @@
+package edu.guilford;
+
+import java.util.Scanner;
+
+/**
+ * Hello world!
+ *
+ */
+public class User 
+{
+    /**
+     * @param args
+     */
+    
+     public static void main( String[] args )
+     {
+        
+
+       
+        
+    
+    
+
+}
+}
